ce58732f1b4a0982a0d3b7c26bc8e891ff3b1cee
espuleta0@gmail.com <espuleta0@gmail.com@4675bb64-d5ec-e01a-8225-18d13d09826a>
2012-06-03 04:04:28 +0000

    -Migrando para Visual Studio 2010 em C#
    thread de teste com atualização para sinais funcionando!

---
+++ trunk/grafico_teste/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using thread_chart;
+using System.Threading;
+
+namespace grafico_teste
+{
+    public partial class Form1 : Form
+    {
+        delegate void SetTextCallback(double x, double y);
+        private Thread ThreadChart;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*
+             * //esse codigo faz limitar o gráfico. 
+             * chart1.ChartAreas.Add("area");
+            chart1.ChartAreas["area"].AxisX.Minimum = 0;
+            chart1.ChartAreas["area"].AxisX.Maximum = 50;
+            chart1.ChartAreas["area"].AxisY.Minimum = 0;
+            chart1.ChartAreas["area"].AxisX.Maximum = 100;*/
+
+            atualiza_sinal objCliente = new atualiza_sinal(chart1);
+            ThreadChart = new Thread(new ThreadStart(objCliente.Inicializa));
+            ThreadChart.Start();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            ThreadChart.Abort();
+        }
+    }
+}
