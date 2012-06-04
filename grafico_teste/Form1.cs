668be669659b1eac9bc2012b9c9085f27735d878
espuleta0@gmail.com <espuleta0@gmail.com@4675bb64-d5ec-e01a-8225-18d13d09826a>
2012-06-04 01:41:20 +0000

    -Melhoras ao inserir cursores -Adicionando texto ao gráfico
    -Adicionado arquivo para altear aparencia VS 2010

--- grafico_teste/Form1.cs
+++ grafico_teste/Form1.cs
@@ -9,6 +9,7 @@ using System.Windows.Forms;
 using thread_chart;
 using System.Threading;
 using System.Windows;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace grafico_teste
 {
@@ -17,12 +18,14 @@ namespace grafico_teste
         delegate void SetTextCallback(double x, double y);
         private Thread ThreadChart;
         private int suspender = 0;
+        private int numCursor = 0;
+        private double x_Pos, y_Pos;
 
         public Form1()
         {
             InitializeComponent();
         }
-
+        //-----------------------------------------------------------------
         private void button1_Click(object sender, EventArgs e)
         {
             /*
@@ -32,20 +35,27 @@ namespace grafico_teste
             chart1.ChartAreas["area"].AxisX.Maximum = 50;
             chart1.ChartAreas["area"].AxisY.Minimum = 0;
             chart1.ChartAreas["area"].AxisX.Maximum = 100;*/
-
+            
+            chart1.Enabled = true;
             atualiza_sinal objCliente = new atualiza_sinal(chart1);
             ThreadChart = new Thread(new ThreadStart(objCliente.Inicializa));
             ThreadChart.Start();
             btn_Suspender.Visible = true;
+            btnLigar.Visible = false;
         }
-
+        //-----------------------------------------------------------------
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
+            if (suspender != 0)
+            {
+                ThreadChart.Resume();
+            }
             ThreadChart.Abort();
         }
-
+        //-----------------------------------------------------------------
         private void button2_Click(object sender, EventArgs e)
         {
+            
             if (suspender == 0)
             {
                 ThreadChart.Suspend();
@@ -59,19 +69,77 @@ namespace grafico_teste
                 btn_Suspender.Text = "Suspender";
             }
         }
-
-        private void chart1_MouseMove(object sender, System.Windows.Forms.MouseEventArgs e)
+        //-----------------------------------------------------------------
+        private void chart1_MouseMove(object sender, MouseEventArgs e)
         {
-            Point chartLocationOnForm = chart1.FindForm().PointToClient(chart1.Parent.PointToScreen(chart1.Location));
-            chart1.ChartAreas[0].CursorX.SetCursorPixelPosition(new PointF(e.X - chartLocationOnForm.X, e.Y - chartLocationOnForm.Y), true);
-            chart1.ChartAreas[0].CursorY.SetCursorPixelPosition(new PointF(e.X - chartLocationOnForm.X, e.Y - chartLocationOnForm.Y), true);
+            if (numCursor == 0)
+            {
 
-            double x = chart1.ChartAreas[0].CursorX.Position;
-            double y = chart1.ChartAreas[0].CursorY.Position;
+                x_Pos = chart1.ChartAreas[0].AxisX.PixelPositionToValue(e.X);
+                y_Pos = chart1.ChartAreas[0].AxisY.PixelPositionToValue(e.Y);
 
-            lbl_x.Text = x.ToString();
-            lbl_Y.Text = y.ToString();
+                //linha fixa
+                VerticalLineAnnotation cursor_vertical = new VerticalLineAnnotation();
+                cursor_vertical.AnchorDataPoint = chart1.Series[0].Points[2];
+                cursor_vertical.Height = 81;
+               
+                cursor_vertical.LineColor = Color.Blue;
+                cursor_vertical.LineWidth = 2;
+                cursor_vertical.AnchorX = x_Pos;
+                chart1.Annotations.Add(cursor_vertical);
+	            
+                //Anotação "flag"
+                
+                TextAnnotation annotation = new TextAnnotation();
+                annotation.AnchorDataPoint = chart1.Series[0].Points[2];
+                annotation.AnchorX =  x_Pos ;
+                annotation.AnchorY = y_Pos; 
+            
+                annotation.Text = "Flag 1";
+                annotation.ForeColor = Color.DarkBlue;
+                annotation.Font = new Font("Arial", 9);
+                chart1.Annotations.Add(annotation);
+                
+                numCursor++;
+            }
+            else if (numCursor == 1)
+            {
+                chart1.ChartAreas[0].CursorX.LineColor = Color.Chocolate;
+                chart1.ChartAreas[0].CursorX.LineWidth = 2;
+                chart1.ChartAreas[0].CursorX.SetCursorPixelPosition(new PointF(e.X, e.Y), true);
+
+                double x = chart1.ChartAreas[0].AxisX.PixelPositionToValue(e.X);
+                double y = chart1.ChartAreas[0].AxisY.PixelPositionToValue(e.Y);
+
+                //// Set range selection color, specifying transparency of 120
+                //chart1.ChartAreas[0].CursorX.SelectionColor = Color.FromArgb(120, 50, 50, 50);
+                //chart1.ChartAreas[0].CursorX.IsUserEnabled = true;
+                //chart1.ChartAreas[0].CursorX.IsUserSelectionEnabled = true;
+                //chart1.ChartAreas[0].CursorX.SetSelectionPosition(x_Pos, chart1.ChartAreas[0].AxisX.PixelPositionToValue(e.X));
+
+                TextAnnotation annotation = new TextAnnotation();
+                annotation.AnchorDataPoint = chart1.Series[0].Points[2];
+                annotation.AnchorX = x;
+                annotation.AnchorY = y;
+
+                annotation.Text = "Flag 2";
+                annotation.ForeColor = Color.Green;
+                annotation.Font = new Font("Arial", 9);
+
+                chart1.Annotations.Add(annotation);
+                numCursor++;//CLICAR + VEZES SEM EFEITO
+            }
          
+        }
+        //-----------------------------------------------------------------
+        private void mouse_Mover(object sender, MouseEventArgs e)
+        {
+                double x = chart1.ChartAreas[0].AxisX.PixelPositionToValue(e.X);
+                double y = chart1.ChartAreas[0].AxisY.PixelPositionToValue(e.Y);
+                lbl_x.Text = x.ToString();
+                lbl_Y.Text = y.ToString();
+                if(numCursor < 2)
+                    chart1.ChartAreas[0].CursorX.SetCursorPosition(x);
         }
     }
 }
