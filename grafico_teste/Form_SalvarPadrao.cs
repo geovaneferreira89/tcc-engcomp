513124c379099439a7d0ce3c518985aac272805b
geovanevinicius89@gmail.com <geovanevinicius89@gmail.com@4675bb64-d5ec-e01a-8225-18d13d09826a>
2012-11-13 13:38:23 +0000

    #Padroes
    ->Salvando padroes selecionado em arquivos de texto

---
+++ grafico_teste/Form_SalvarPadrao.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AmbienteRPB
+{
+    public partial class Form_SalvarPadrao : Form
+    {
+        //Atributo que pode ser acessado pelas outras classes, no caso clase GerenArquivos...
+        public String NomePadrao
+        {
+            get;
+            set;
+        }
+        public Form_SalvarPadrao()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_salvar_Click(object sender, EventArgs e)
+        {
+            NomePadrao = text_NomePadrao.Text;
+            this.Close();
+        }
+    }
+}
