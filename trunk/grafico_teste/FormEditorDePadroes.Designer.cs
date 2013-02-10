6abaa1176dd2b2931453d681f89be7e36d3ae1ef
geovanevinicius89@gmail.com <geovanevinicius89@gmail.com@4675bb64-d5ec-e01a-8225-18d13d09826a>
2013-02-10 01:32:39 +0000

    Editor, melhoras, porem com bugs...

--- trunk/grafico_teste/FormEditorDePadroes.Designer.cs
+++ trunk/grafico_teste/FormEditorDePadroes.Designer.cs
@@ -30,27 +30,27 @@
         {
             this.openFile = new System.Windows.Forms.OpenFileDialog();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.btnSalvar = new System.Windows.Forms.Button();
-            this.btn_Abrir = new System.Windows.Forms.Button();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.groupBox5 = new System.Windows.Forms.GroupBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.edtEvento_Nome = new System.Windows.Forms.TextBox();
-            this.btnSair = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.btnMarcar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btn_Abrir = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.groupBox1.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
-            this.groupBox4.SuspendLayout();
-            this.groupBox5.SuspendLayout();
             this.SuspendLayout();
             // 
             // openFile
@@ -67,72 +67,6 @@
             this.groupBox1.Size = new System.Drawing.Size(127, 450);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(this.btnMarcar);
-            this.groupBox2.Controls.Add(this.btnSair);
-            this.groupBox2.Controls.Add(this.btnSalvar);
-            this.groupBox2.Controls.Add(this.btn_Abrir);
-            this.groupBox2.Location = new System.Drawing.Point(9, 15);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(111, 124);
-            this.groupBox2.TabIndex = 7;
-            this.groupBox2.TabStop = false;
-            // 
-            // btnSalvar
-            // 
-            this.btnSalvar.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnSalvar.Location = new System.Drawing.Point(14, 40);
-            this.btnSalvar.Name = "btnSalvar";
-            this.btnSalvar.Size = new System.Drawing.Size(82, 22);
-            this.btnSalvar.TabIndex = 8;
-            this.btnSalvar.Text = "Salvar";
-            this.btnSalvar.UseVisualStyleBackColor = true;
-            // 
-            // btn_Abrir
-            // 
-            this.btn_Abrir.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btn_Abrir.Location = new System.Drawing.Point(14, 15);
-            this.btn_Abrir.Name = "btn_Abrir";
-            this.btn_Abrir.Size = new System.Drawing.Size(82, 21);
-            this.btn_Abrir.TabIndex = 7;
-            this.btn_Abrir.Text = "Abrir Padrão ";
-            this.btn_Abrir.UseVisualStyleBackColor = true;
-            this.btn_Abrir.Click += new System.EventHandler(this.btn_Abrir_Click);
-            // 
-            // groupBox3
-            // 
-            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox3.Controls.Add(this.chart1);
-            this.groupBox3.Location = new System.Drawing.Point(149, 2);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(505, 450);
-            this.groupBox3.TabIndex = 3;
-            this.groupBox3.TabStop = false;
-            // 
-            // chart1
-            // 
-            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.chart1.Location = new System.Drawing.Point(10, 11);
-            this.chart1.Name = "chart1";
-            this.chart1.Size = new System.Drawing.Size(486, 430);
-            this.chart1.TabIndex = 2;
-            this.chart1.Text = "chart1";
-            // 
-            // groupBox4
-            // 
-            this.groupBox4.Controls.Add(this.edtEvento_Nome);
-            this.groupBox4.Controls.Add(this.label3);
-            this.groupBox4.Location = new System.Drawing.Point(10, 145);
-            this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(110, 65);
-            this.groupBox4.TabIndex = 8;
-            this.groupBox4.TabStop = false;
             // 
             // groupBox5
             // 
@@ -184,6 +118,23 @@
             this.textBox1.TabIndex = 6;
             this.textBox1.Text = "3";
             // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.edtEvento_Nome);
+            this.groupBox4.Controls.Add(this.label3);
+            this.groupBox4.Location = new System.Drawing.Point(10, 145);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(110, 65);
+            this.groupBox4.TabIndex = 8;
+            this.groupBox4.TabStop = false;
+            // 
+            // edtEvento_Nome
+            // 
+            this.edtEvento_Nome.Location = new System.Drawing.Point(8, 34);
+            this.edtEvento_Nome.Name = "edtEvento_Nome";
+            this.edtEvento_Nome.Size = new System.Drawing.Size(93, 20);
+            this.edtEvento_Nome.TabIndex = 1;
+            // 
             // label3
             // 
             this.label3.AutoSize = true;
@@ -193,12 +144,28 @@
             this.label3.TabIndex = 0;
             this.label3.Text = "Evento";
             // 
-            // edtEvento_Nome
+            // groupBox2
             // 
-            this.edtEvento_Nome.Location = new System.Drawing.Point(8, 34);
-            this.edtEvento_Nome.Name = "edtEvento_Nome";
-            this.edtEvento_Nome.Size = new System.Drawing.Size(93, 20);
-            this.edtEvento_Nome.TabIndex = 1;
+            this.groupBox2.Controls.Add(this.btnMarcar);
+            this.groupBox2.Controls.Add(this.btnSair);
+            this.groupBox2.Controls.Add(this.btnSalvar);
+            this.groupBox2.Controls.Add(this.btn_Abrir);
+            this.groupBox2.Location = new System.Drawing.Point(9, 15);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(111, 124);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            // 
+            // btnMarcar
+            // 
+            this.btnMarcar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnMarcar.Location = new System.Drawing.Point(14, 95);
+            this.btnMarcar.Name = "btnMarcar";
+            this.btnMarcar.Size = new System.Drawing.Size(82, 22);
+            this.btnMarcar.TabIndex = 10;
+            this.btnMarcar.Text = "Marcar";
+            this.btnMarcar.UseVisualStyleBackColor = true;
+            this.btnMarcar.Click += new System.EventHandler(this.btnMarcar_Click);
             // 
             // btnSair
             // 
@@ -211,15 +178,52 @@
             this.btnSair.UseVisualStyleBackColor = true;
             this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
             // 
-            // btnMarcar
+            // btnSalvar
             // 
-            this.btnMarcar.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnMarcar.Location = new System.Drawing.Point(14, 95);
-            this.btnMarcar.Name = "btnMarcar";
-            this.btnMarcar.Size = new System.Drawing.Size(82, 22);
-            this.btnMarcar.TabIndex = 10;
-            this.btnMarcar.Text = "Marcar";
-            this.btnMarcar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnSalvar.Enabled = false;
+            this.btnSalvar.Location = new System.Drawing.Point(14, 40);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(82, 22);
+            this.btnSalvar.TabIndex = 8;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btn_Abrir
+            // 
+            this.btn_Abrir.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_Abrir.Location = new System.Drawing.Point(14, 15);
+            this.btn_Abrir.Name = "btn_Abrir";
+            this.btn_Abrir.Size = new System.Drawing.Size(82, 21);
+            this.btn_Abrir.TabIndex = 7;
+            this.btn_Abrir.Text = "Abrir Padrão ";
+            this.btn_Abrir.UseVisualStyleBackColor = true;
+            this.btn_Abrir.Click += new System.EventHandler(this.btn_Abrir_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.chart1);
+            this.groupBox3.Location = new System.Drawing.Point(149, 2);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(505, 450);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.chart1.Location = new System.Drawing.Point(10, 11);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(486, 430);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            this.chart1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.chart1_MouseClick);
             // 
             // FormEditorDePadroes
             // 
@@ -232,13 +236,13 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Editor de Eventos";
             this.groupBox1.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox3.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
-            this.groupBox4.ResumeLayout(false);
-            this.groupBox4.PerformLayout();
-            this.groupBox5.ResumeLayout(false);
-            this.groupBox5.PerformLayout();
             this.ResumeLayout(false);
 
         }
--- trunk/grafico_teste/FormEditorDePadroes.cs
+++ trunk/grafico_teste/FormEditorDePadroes.cs
@@ -6,72 +6,152 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.IO;
+using System.Runtime.InteropServices;
 
 namespace AmbienteRPB
 {
     public partial class FormEditorDePadroes : Form
     {
+        private int numCursor = 0;
+        private int mostrarCursores = 0;
+        private float x_Pos, y_Pos;
+        private HitTestResult var_result;
+        PointF Padrao_Inicio;
+        PointF Padrao_Fim;
         private GerenArquivos Arquivos;
+        //-----------------------------------------------------
         public FormEditorDePadroes()
         {
             InitializeComponent();
         }
-
+        //-----------------------------------------------------
         private void btn_Abrir_Click(object sender, EventArgs e)
         {
-            if (openFile.ShowDialog() == DialogResult.OK)
-            {
-                chart1.ChartAreas.Add("Padrao");
-                chart1.Series.Add("Serie01");
-                chart1.Series["Serie01"].ChartArea = "Padrao";
-                chart1.Series["Serie01"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-
-                // Arquivos.ImportarPadraoArquivo(openFile.FileName, chart1);
-                // chart = _Chart as System.Windows.Forms.DataVisualization.Charting.Chart;
-                System.IO.StreamReader fileR = new System.IO.StreamReader(openFile.FileName);
-                string dados;
-                dados = fileR.ReadLine();
-                dados = dados.Substring(22);
-                dados = dados.Substring(0, dados.Length - 1);
-                int NumeroDeAmostras = Convert.ToInt16(dados);
-                chart1.Series[0].Color = Color.Red;
-
-                for (int i = 0; i <= NumeroDeAmostras; i++)
+           
+                if (openFile.ShowDialog() == DialogResult.OK)
                 {
+                    chart1.ChartAreas.Add("Padrao");
+                    chart1.Series.Add("Serie01");
+                    chart1.Series["Serie01"].ChartArea = "Padrao";
+                    chart1.Series["Serie01"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+
+                    // Arquivos.ImportarPadraoArquivo(openFile.FileName, chart1);
+                    // chart = _Chart as System.Windows.Forms.DataVisualization.Charting.Chart;
+                    System.IO.StreamReader fileR = new System.IO.StreamReader(openFile.FileName);
+                    edtEvento_Nome.Text = openFile.SafeFileName; //Tirar o resto... 
+                    string dados;
                     dados = fileR.ReadLine();
-                    string x = dados;
-                    string y = dados;
-                    int X_ = x.IndexOf(" ");
+                    dados = dados.Substring(22);
+                    dados = dados.Substring(0, dados.Length - 1);
+                    int NumeroDeAmostras = Convert.ToInt16(dados);
+                    chart1.Series[0].Color = Color.Red;
 
-                    x = x.Substring(3);
-                    x = x.Substring(0, X_-4);
+                    for (int i = 0; i <= NumeroDeAmostras; i++)
+                    {
+                        dados = fileR.ReadLine();
+                        string x = dados;
+                        string y = dados;
+                        int X_ = x.IndexOf(" ");
 
-                    y = y.Substring(X_ + 3);
-                    y = y.Substring(0, y.Length - 1);
+                        x = x.Substring(3);
+                        x = x.Substring(0, X_ - 4);
+
+                        y = y.Substring(X_ + 3);
+                        y = y.Substring(0, y.Length - 1);
 
 
 
-                    chart1.Series[0].Points.AddXY(Convert.ToDouble(x), Convert.ToDouble(y));
+                        chart1.Series[0].Points.AddXY(Convert.ToDouble(x), Convert.ToDouble(y));
+                    }
+
                 }
-                
-            }
         }
+        //-----------------------------------------------------
         //Frequencia
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
             if(textBox1.Text != null)
             chart1.ChartAreas[0].AxisX.ScaleView.Size = Convert.ToDouble(textBox1.Text) ;
         }
+        //-----------------------------------------------------
         //Amplitude
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
             if (textBox2.Text != null)
             chart1.ChartAreas[0].AxisY.ScaleView.Size = Convert.ToDouble(textBox2.Text);
         }
-
+        //-----------------------------------------------------
         private void btnSair_Click(object sender, EventArgs e)
         {
             this.Close();
         }
+        //-----------------------------------------------------
+        private void btnMarcar_Click(object sender, EventArgs e)
+        {
+            //limpa seleção
+        }
+        //-----------------------------------------------------
+        private void chart1_MouseClick(object sender, MouseEventArgs e)
+        {
+            //HitTestResult result = chart1.HitTest(e.X, e.Y);
+            if (chart1.ChartAreas[0] != null)
+            {
+                if (numCursor == 0)
+                {
+                    
+                    chart1.Annotations.Clear();
+                    chart1.ChartAreas[0].CursorX.SelectionColor = Color.FromArgb(00, 50, 50, 50);
+                    chart1.ChartAreas[0].CursorX.SetCursorPixelPosition(new PointF(0, 0), false);
+                    x_Pos = (e.X);
+                    y_Pos = (e.Y);
+                    //linha fixa
+                    VerticalLineAnnotation cursor_vertical = new VerticalLineAnnotation();
+                    cursor_vertical.AnchorDataPoint = chart1.Series[0].Points[1];
+                    cursor_vertical.Height = chart1.ChartAreas[0].Position.Height;
+                    cursor_vertical.LineColor = Color.Green;
+                    cursor_vertical.LineWidth = 1;
+                    cursor_vertical.AnchorX = chart1.ChartAreas[0].AxisX.PixelPositionToValue(e.X);
+                    cursor_vertical.AnchorY = chart1.ChartAreas[0].AxisY.Maximum;
+                    chart1.Annotations.Add(cursor_vertical);
+                    numCursor++;
+                }
+                else if (numCursor == 1)
+                {
+                    chart1.ChartAreas[0].CursorX.AxisType = AxisType.Secondary;
+                    chart1.ChartAreas[0].CursorX.LineColor = Color.Green;
+                    chart1.ChartAreas[0].CursorX.LineWidth = 1;
+                    chart1.ChartAreas[0].CursorX.SetCursorPixelPosition(new PointF(e.X, e.Y), true);
+                    // Set range selection color, specifying transparency of 120
+                    chart1.ChartAreas[0].CursorX.SelectionColor = Color.FromArgb(90, 50, 50, 50);
+                    chart1.ChartAreas[0].CursorX.IsUserEnabled = true;
+                    chart1.ChartAreas[0].CursorX.IsUserSelectionEnabled = true;
+                    Padrao_Inicio = new PointF(x_Pos, y_Pos);
+                    Padrao_Fim = new PointF(e.X, e.Y);
+                    chart1.ChartAreas[0].CursorX.SetSelectionPixelPosition(Padrao_Inicio, Padrao_Fim, true);
+                    numCursor = 0;//CLICAR + VEZES SEM EFEITO
+                    btnSalvar.Enabled = true;
+                    Padrao_Inicio.X = (float)chart1.ChartAreas[0].AxisX.PixelPositionToValue(x_Pos);
+                    Padrao_Fim.X = (float)chart1.ChartAreas[0].AxisX.PixelPositionToValue(e.X);
+                }
+            }
+        }
+        //-----------------------------------------------------
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            btnSalvar.Enabled = false;
+            numCursor = 0;
+            Exportar_Padrao(Padrao_Inicio, Padrao_Fim);
+            chart1.Annotations.Clear();
+        }
+        //-----------------------------------------------------
+        private void Exportar_Padrao(PointF Padrao_Inicio, PointF Padrao_Fim)
+        {
+            Arquivos = new GerenArquivos();
+            Arquivos.ExportarPadraoEditado(edtEvento_Nome.Text, chart1, Padrao_Inicio, Padrao_Fim);
+            MessageBox.Show("Padrão '" + edtEvento_Nome.Text + "' editado e salvo.", "Ambiente RPB");
+        }
+        //-----------------------------------------------------
     }
 }
