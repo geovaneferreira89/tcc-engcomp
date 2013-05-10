09fdd210b90ed503050843e72553ce2a30b2f4bd
geovanevinicius89@gmail.com <geovanevinicius89@gmail.com@4675bb64-d5ec-e01a-8225-18d13d09826a>
2013-05-10 01:02:39 +0000

    #Editor de Eventos - Sinal Plotado estava errado (Geovane)   Estava salvando a posição x e y do mouse, e nao o valor correto que está no chart.
    #Instalador
    - Gerando versão

--- trunk/Source/FormEditorDeEventos.cs
+++ trunk/Source/FormEditorDeEventos.cs
@@ -21,29 +21,29 @@ namespace AmbienteRPB
         private int mostrarCursores = 0;
         private float x_Pos, y_Pos;
         private HitTestResult var_result;
-        private Chart Chart_Principal;
         private string EDF_File;
         PointF Padrao_Inicio;
         PointF Padrao_Fim;
         EdfFile SinalEEG;
-
+        //---------------------------------------------------------------------------
         public FormEditorDeEventos(ListaPadroesEventos[] _Listas, string _EDF_File)
         {
             InitializeComponent();            
             Listas = _Listas;
-           // Chart_Principal = _Chart_Principal;
             EDF_File = _EDF_File;
         }
-     
+        //---------------------------------------------------------------------------
         private void button1_Click(object sender, EventArgs e)
         {
             this.Close();
         }
+        //---------------------------------------------------------------------------
         private void FormEditorDeEventos_Load(object sender, EventArgs e)
         {
             for (int i = 0; i < 20; i++)
                 comboTiposDeEventos.Items.Add(Listas[i].NomePadrao);
         }
+        //---------------------------------------------------------------------------
         private void comboTiposDeEventos_SelectedIndexChanged(object sender, EventArgs e)
         {
             lbxEventosPorTipo.Items.Clear();
@@ -55,6 +55,7 @@ namespace AmbienteRPB
                 chart1.ChartAreas.Remove(chart1.ChartAreas[0]);
             }
         }
+        //---------------------------------------------------------------------------
         private void lbxEventosPorTipo_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (chart1.Series.Count != 0)
@@ -81,16 +82,15 @@ namespace AmbienteRPB
                 //Nome do canal
                 string nome_canal =  Listas[comboTiposDeEventos.SelectedIndex].GetNomesEvento(lbxEventosPorTipo.SelectedIndex);
                 int X_ = nome_canal.IndexOf("_");
-              //  nome_canal = nome_canal.Substring(3);
                 nome_canal = nome_canal.Substring(X_+1);
                 //Inicio do enveto
-                float x = Listas[comboTiposDeEventos.SelectedIndex].GetValorInicio(lbxEventosPorTipo.SelectedIndex).X;
-                float y = Listas[comboTiposDeEventos.SelectedIndex].GetValorInicio(lbxEventosPorTipo.SelectedIndex).Y;
-                chart1.Series[0].Points.AddY(Convert.ToDouble(y));
+                float x = Listas[comboTiposDeEventos.SelectedIndex].GetValorInicio(lbxEventosPorTipo.SelectedIndex).X - 1;
+                float x_max = Listas[comboTiposDeEventos.SelectedIndex].GetValorFim(lbxEventosPorTipo.SelectedIndex).X + 1;
+                
                 float aux;
                 int DataRecords_lidos = 0;
                 int tempo_X = DataRecords_lidos * 256;
-                while (tempo_X <= Listas[comboTiposDeEventos.SelectedIndex].GetValorFim(lbxEventosPorTipo.SelectedIndex).X)
+                while (tempo_X <= (int) x_max)
                 {
                     DataRecords_lidos++;
                     SinalEEG.ReadDataBlock(DataRecords_lidos);
@@ -100,8 +100,9 @@ namespace AmbienteRPB
                         for (int i = 0; i < 256; i++)
                         {
                             if (SinalEEG.SignalInfo[j].SignalLabel == nome_canal)
-                            {    
-                                if(tempo_X >= x){
+                            {
+                                if (tempo_X >= (int) x && tempo_X <= (int) x_max)
+                                {
                                     chart1.Series[0].Points.AddY(SinalEEG.DataBuffer[SinalEEG.SignalInfo[j].BufferOffset + i]);
                                 }
                                 else
@@ -115,7 +116,7 @@ namespace AmbienteRPB
                 }
             }
         }
-
+        //---------------------------------------------------------------------------
         private void chart1_MouseClick(object sender, MouseEventArgs e)
         {
             if (chart1.ChartAreas[0] != null)
@@ -158,7 +159,7 @@ namespace AmbienteRPB
                 }
             }
         }
-        //---------
+        //---------------------------------------------------------------------------
         private void btnSalvar_Click(object sender, EventArgs e)
         {
             btnSalvar.Enabled = false;
@@ -166,30 +167,31 @@ namespace AmbienteRPB
             Exportar_Padrao(Padrao_Inicio, Padrao_Fim);
             chart1.Annotations.Clear();
         }
-        //---------
+        //---------------------------------------------------------------------------
         private void Exportar_Padrao(PointF Padrao_Inicio, PointF Padrao_Fim)
         {
             Arquivos = new GerenArquivos();
             Arquivos.Exportar_Padroes_Eventos(Listas);
             MessageBox.Show("Padrão '" + edtEvento_Nome.Text + "' editado e salvo.", "Ambiente RPB");
         }
-        //--
+        //---------------------------------------------------------------------------
         private void cbx_Inicio_Click(object sender, EventArgs e)
         {
             cbx_Referencia.Checked = false;
             cbx_Fim.Checked = false;
         }
-        //--
+        //---------------------------------------------------------------------------        
         private void cbx_Fim_Click(object sender, EventArgs e)
         {
             cbx_Inicio.Checked = false;
             cbx_Referencia.Checked = false;
         }
-        //--
+        //---------------------------------------------------------------------------        
         private void cbx_Referencia_Click(object sender, EventArgs e)
         {
             cbx_Inicio.Checked = false;
             cbx_Fim.Checked = false;
         }
+        //---------------------------------------------------------------------------
     }
 }
