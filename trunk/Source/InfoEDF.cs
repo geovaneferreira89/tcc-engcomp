1fc0d00b37efbf75b1c547933773d3f95da10078
geovanevinicius89@gmail.com <geovanevinicius89@gmail.com@4675bb64-d5ec-e01a-8225-18d13d09826a>
2013-06-18 03:03:48 +0000

    -Montagens de canais, agora as montagens, melhor dizendo o layer ou titulo de cada série aparece corretamente, ex FP1-CZ ...

---
+++ trunk/Source/InfoEDF.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using NeuroLoopGainLibrary.Edf;
+
+namespace AmbienteRPB
+{
+    public partial class InfoEDF : Form
+    {
+        private EdfFile EDF;
+        public InfoEDF(EdfFile EDF_)
+        {
+            EDF =  EDF_;
+            InitializeComponent();
+        }
+
+        private void InfoEDF_Load(object sender, EventArgs e)
+        {
+
+           label1.Text = "Nome do Paciente: " + EDF.FileInfo.Patient;
+           label2.Text = "Nascimento: " + EDF.FileInfo.PatientBirthDate;
+           label3.Text = "Sexo: " + EDF.FileInfo.PatientGender;
+           label4.Text = "Outras Informações: " + EDF.FileInfo.PatientOtherInfo;
+           label5.Text = "Hora: " + EDF.FileInfo.StartDate;
+           label6.Text = "Duração: " + EDF.FileInfo.NrDataRecords;
+           label7.Text = "Inicio do Exame: " + EDF.FileInfo.StartTime;
+           label8.Text = "Fim do Exame: " + EDF.FileInfo.EndDateTime;
+           label9.Text = "Version: " + EDF.FileInfo.Version;
+
+           label10.Text = EDF.FileInfo.Recording;
+
+        }
+    }
+}
