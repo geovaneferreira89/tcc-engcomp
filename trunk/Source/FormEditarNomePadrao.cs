a7b501fdde512330059cf60ea9e2b775d7b7df01
geovanevinicius89@gmail.com <geovanevinicius89@gmail.com@4675bb64-d5ec-e01a-8225-18d13d09826a>
2013-07-16 22:40:09 +0000

    Opção de seleção de numero de vetores e o tamanho de vetores

--- trunk/Source/FormEditarNomePadrao.cs
+++ trunk/Source/FormEditarNomePadrao.cs
@@ -12,7 +12,23 @@ namespace AmbienteRPB
     public partial class FormEditarNomePadrao : Form
     {
         //Atributo que pode ser acessado pelas outras classes, no caso clase GerenArquivos...
+        public int opcao
+        {
+            get;
+            set;
+
+        }
         public String NomePadrao
+        {
+            get;
+            set;
+        }
+        public int Vetores
+        {
+            get;
+            set;
+        }
+        public int TamVetores
         {
             get;
             set;
@@ -24,10 +40,27 @@ namespace AmbienteRPB
 
         private void btn_salvar_Click(object sender, EventArgs e)
         {
-            if (text_NomePadrao.Text != "")
+            if (opcao == 1)
+            {
+                Vetores = Convert.ToInt16(text_NomePadrao.Text);
+                TamVetores = Convert.ToInt16(txt_VetorTamanho.Text);
+                this.Close();
+            }
+            else if (text_NomePadrao.Text != "")
             {
                 NomePadrao = text_NomePadrao.Text;
                 this.Close();
+            }
+        }
+
+        private void FormEditarNomePadrao_Shown(object sender, EventArgs e)
+        {
+            if (opcao == 1)
+            {
+                this.Height = 93;
+                lbl_digiteONomeDoPadrao.Text = "Num Vet";
+                text_NomePadrao.Text = Convert.ToString(Vetores);
+                txt_VetorTamanho.Text = Convert.ToString(TamVetores);
             }
         }
     }
