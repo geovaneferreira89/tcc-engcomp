483c95613dff6afeb83ae238a172a850c8797714
ge.dwcz@gmail.com <ge.dwcz@gmail.com@4675bb64-d5ec-e01a-8225-18d13d09826a>
2013-09-17 14:52:14 +0000

    módulo avaliador

---
+++ trunk/avaliador/avaliar/teste/teste/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Cria entradas
+            List<double> resultados = new List<double> {0, 0, 0, 1, 0, 0, 0, 1};
+            List<double> marcacoes = new List<double> {0, 0, 0, 1, 0, 0, 0, 1};
+
+
+            //Avalia
+            Avaliador avaliador = new Avaliador();
+            avaliador.Avaliar(resultados, marcacoes);
+
+
+            //Saída
+            Console.WriteLine("vamos testar");
+            for (int i = 0; i < avaliador.VP.Count(); i++)
+            {
+                Console.WriteLine("VP " + i + ": " + avaliador.VP[i]);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
